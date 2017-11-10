788c3bbce18ff96b1422bcf7eb44d39a3e90820b
alodavi <aloisia.davi@gmail.com>
2017-11-10 12:24:07 +0100

    added services

--- app/services/UrlServices.scala
+++ app/services/UrlServices.scala
@@ -1,5 +1,9 @@
 package services
 
+import java.net.URL
+
+import scala.util.{Failure, Success, Try}
+
 object UrlServices {
 
   def generateTinyUrl(inputUrl:String, baseUrl: String):String = {
@@ -7,7 +11,14 @@ object UrlServices {
   }
 
   def randomNumber(url:String):Int={
-     Math.floor(100000 + Math.random() * 900000).toInt
+    validate(url) match {
+      case Success(valid) => Math.floor(100000 + Math.random() * 900000).toInt
+      case Failure(error) => throw new Exception("Invalid URL") //todo specific exception
+    }
+  }
+
+  def validate(url:String)={
+    Try { new URL(url) }
   }
 
 }
